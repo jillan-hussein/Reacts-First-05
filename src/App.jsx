6e2a6622e4350236d12baea657176dd9ce7cc1e2
Jillan Hussein <JillanHussein@gamil.com>
2026-08-13 18:40:25 +0300

    AddPages_without Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,75 @@
-import Header from "./header"
-import Footer from './footer'
-import Alert from "./Components/molecules/Alert"
-import Button from "./Components/atoms/Button"
-import Counter from "./Components/molecules/Counter"
-import UserData from "./Components/molecules/UserData"
-import MiniForm from "./Components/molecules/MiniForm"
+import Header from "./header";
+import Footer from "./footer";
+import Alert from "./Components/molecules/Alert";
+import Button from "./Components/atoms/Button";
+import Counter from "./Components/molecules/Counter";
+import UserData from "./Components/molecules/UserData";
+import MiniForm from "./Components/molecules/MiniForm";
+import Home from "./Components/pages/Home";
+import Contact from "./Components/pages/Contact";
+import About from "./Components/pages/About";
+import { useState } from "react";
 
-export default
-function App() {
-    return(
-        <div className="p-4">
-        <Header />
-        <MiniForm />
-        <UserData />
-        <Counter />
-        <section>
-            <Button title="+" />
-            <span>0</span>
-            <Button title="-" />
-        </section>
-        <section className="p-4">
-            <Button title="Save" />
-        </section>
-        <section>
+export default function App() {
+  const [page, setPage] = useState("Home");
+
+  return (
+    <div className="p-4">
+      <div>
+        <nav className="bg-gray-900 text-gray-100 p-2 flex gap-3">
+          <button onClick={() => setPage("Home")}>Home</button>
+          <button onClick={() => setPage("About")}>About</button>
+          <button onClick={() => setPage("Contact")}>Contact</button>
+        </nav>
+
+        {page === "Home" && <Home />}
+        {page === "About" && <About />}
+        {page === "Contact" && <Contact />}
+      </div>
+
+      <Header />
+      <MiniForm />
+      <UserData />
+      <Counter />
+      <section>
+        <Button title="+" />
+        <span>0</span>
+        <Button title="-" />
+      </section>
+      <section className="p-4">
+        <Button title="Save" />
+      </section>
+      <section>
         <Alert>
-            <h2 className="text-xl font-bold text-green-800">Lorem, ipsum.</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-            <div className="text-end">
-            <Button variant="green" title="Ok" fontSize="sm" fontColor="white" />
-            </div>
+          <h2 className="text-xl font-bold text-green-800">Lorem, ipsum.</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          <div className="text-end">
+            <Button
+              variant="green"
+              title="Ok"
+              fontSize="sm"
+              fontColor="white"
+            />
+          </div>
         </Alert>
         <Alert>
-            <h2 className="text-xl font-bold text-green-800">Lorem, ipsum.</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          <h2 className="text-xl font-bold text-green-800">Lorem, ipsum.</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
         </Alert>
         <Alert>
-            <h2 className="text-xl font-bold text-green-800">Lorem, ipsum.</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          <h2 className="text-xl font-bold text-green-800">Lorem, ipsum.</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
         </Alert>
         <Alert>
-            <h2 className="text-xl font-bold text-green-800">Lorem, ipsum.</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          <h2 className="text-xl font-bold text-green-800">Lorem, ipsum.</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
         </Alert>
         <Alert>
-            <h2 className="text-xl font-bold text-green-800">Lorem, ipsum.</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          <h2 className="text-xl font-bold text-green-800">Lorem, ipsum.</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
         </Alert>
-        </section>
-        <Footer/>
-        </div>
-    )
+      </section>
+      <Footer />
+    </div>
+  );
 }
---
+++ src/Components/pages/Home.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div>
+      <h2 className="text-4xl font-bold text-blue-950">Home</h2>
+      <p>
+        The contant of Home page <br />
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid
+        asperiores culpa eligendi enim nemo soluta nisi corporis non, quasi,
+        quidem aut molestias facere sequi incidunt sit, dignissimos repellat
+        dolorem ex dicta alias. Tempore eos autem sapiente vel distinctio quia
+        reprehenderit a ducimus ipsum nulla soluta deleniti, mollitia eveniet,
+        atque explicabo officiis. Error exercitationem unde non, rerum iste id
+        officiis provident saepe quidem laboriosam aspernatur. Mollitia
+        reprehenderit atque quas, quidem officiis iusto sunt incidunt iure
+        debitis perspiciatis! Id veniam iusto eligendi sapiente nesciunt placeat
+        magni facilis molestias ab quod nostrum, velit earum dignissimos
+        consectetur nulla perspiciatis officiis! Provident qui commodi possimus?
+      </p>
+    </div>
+  );
+}
